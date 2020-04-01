1b6cc6aa5b1607c051aa9229b73164afd59b2c25
Ilya Korzh <31621462+ikorzh@users.noreply.github.com>
2020-04-01 22:05:47 +0300

    + Add build section draft

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+set -e
+
+echo "Source remote git repo:"
+source <(curl -s https://raw.githubusercontent.com/MaksymSemenykhin/bash_scripts/master/output.sh)
+
+print_info "ls -la"
+
+print_title "NodeJS $(node -v)"
+print_title "NPM $(npm -v)"
+print_title "Yarn $(yarn -v)"
+
+# Build
+print_info "Start build"
+yarn install

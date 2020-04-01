05777102b830aeb87b8aad40b997eb2bc5d0770a
Ilya Korzh <31621462+ikorzh@users.noreply.github.com>
2020-04-01 22:21:39 +0300

    x Fix list folder

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 echo "Source remote git repo:"
 source <(curl -s https://raw.githubusercontent.com/MaksymSemenykhin/bash_scripts/master/output.sh)
 
-print_info "ls -la"
+print_info "$(ls -la)"
 
 print_title "NodeJS $(node -v)"
 print_title "NPM $(npm -v)"
